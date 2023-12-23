7343e9a50782bfe9fa1ae42ffb96a37a4c53a4e0
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-23 10:04:13 -0300

    Swagger

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import {fastify } from 'fastify';
 import { DatabasePostgres } from './database-postgres.js';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 const server = fastify();
 const database = new DatabasePostgres;
+
+await server.register(fastifySwagger, {
+    
+    swagger: {
+      info: {
+        title: 'VideosNode API',
+        description: 'CRUD Videos Node',
+        version: '1.0.0',
+      },
+    },
+    host: "localhost",
+    schemes: ["http", "https"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    tags: [{ name: "Default", description: "Default" }],
+    exposeRoute: true,
+  });
+
+  await server.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+  });
 
 server.post('/videos', async (request, reply) => {
     const { title, description, duration } = request.body;
@@ -37,7 +60,21 @@ server.delete('/videos/:id', async (request, reply) => {
     return reply.status(204).send();
 });
 
-server.listen({
-    host: '0.0.0.0',
-    port: process.env.PORT ?? 3000
-});
+const start = async () => {
+    try {
+      await server.listen({
+        host: '0.0.0.0',
+        port: process.env.PORT ?? 3000
+    });
+        server.ready(err => {
+            if (err) throw err
+            server.swagger()
+        })
+      server.log.info(`Servidor rodando em: ${server.server.address().port}`);
+    } catch (err) {
+        console.error(err)
+        process.exit(1);
+    }
+  };
+  
+  start();
